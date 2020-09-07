0b2b0d81ebc55bf71c89c4e78162d120b5adf8de
Viny <viniciusm896@gmail.com>
2020-09-07 18:15:22 -0300

    design improvments

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const inputElement = document.querySelector('input')
+const noTasks = document.querySelector('.no-tasks')
+const inputElement = document.querySelector('.inputAdd')
 const btnAdd = document.querySelector('.btnAdd')
 const ulElement = document.querySelector('ul')
 
@@ -6,12 +7,14 @@ btnAdd.onclick = ev => {
     ev.preventDefault()
 
     if(inputElement.value){
+        noTasks.classList.add('hidden')
+
         const textElement = document.createElement('span')
         textElement.innerHTML = inputElement.value
 
         const btnRemove = document.createElement('button')
         btnRemove.setAttribute('class', 'btnRemove')
-        btnRemove.innerHTML = 'Remover'
+        btnRemove.innerHTML = `<i class="far fa-times-circle"></i>`
 
         const liElement = document.createElement('li')
         liElement.appendChild(textElement)
